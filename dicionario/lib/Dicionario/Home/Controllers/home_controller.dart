aebd0d3a2cb9072f8baba4ce888f87aba8bf92f4
Yure <yure.siilva9@gmail.com>
2024-05-23 10:42:25 -0300

    create: estado inicial da home com ChangeNotify

--- dicionario/lib/Dicionario/Home/Controllers/home_controller.dart
+++ dicionario/lib/Dicionario/Home/Controllers/home_controller.dart
@@ -1,7 +1,10 @@
-class HomeController {
+import 'package:flutter/material.dart';
+
+class HomeController with ChangeNotifier {
   int? value = 0;
 
   void changeChip(bool selected, int index) {
     value = selected ? index : null;
+    notifyListeners();
   }
 }
--- dicionario/lib/Dicionario/Home/View/home.dart
+++ dicionario/lib/Dicionario/Home/View/home.dart
@@ -1,7 +1,9 @@
+import 'package:dicionario/Dicionario/Home/Controllers/home_controller.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
-  const Home({super.key});
+  Home({super.key});
+  final controller = HomeController();
 
   @override
   State<Home> createState() => _HomeState();
@@ -10,22 +12,58 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    final _availableChoices = [
+    final _Choices = [
       'Word list',
       'History',
       'Favorites',
     ];
 
-    return Scaffold(
-      body: SizedBox.expand(
-        child: ListView(
-          children: [
-            SizedBox(
-              height: 20,
+    return ListenableBuilder(
+        listenable: widget.controller,
+        builder: (context, child) {
+          return Scaffold(
+            body: SizedBox.expand(
+              child: ListView(
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 70,
+                          child: ListView.separated(
+                              scrollDirection: Axis.horizontal,
+                              itemBuilder: (context, index) => ChoiceChip(
+                                    label: Text(_Choices[index]),
+                                    selected: widget.controller.value == index,
+                                    onSelected: (value) {
+                                      widget.controller
+                                          .changeChip(value, index);
+                                    },
+                                  ),
+                              separatorBuilder: (context, index) =>
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                              itemCount: _Choices.length),
+                        ),
+                        widget.controller.value == 0
+                            ? Text("1")
+                            : widget.controller.value == 1
+                                ? Text("2")
+                                : Text("3")
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
-    );
+          );
+        });
   }
 }
